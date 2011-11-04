dd71f1368d2061785dd37461b73d69cc73ad2528
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2011-11-04 23:46:54 +0100

    Include lowercase characters in HP serial numbers

--- lib/classify.rb
+++ lib/classify.rb
@@ -1,7 +1,8 @@
 module WarrantyCheck
   
   VENDOR_PATTERNS = {
-    WarrantyCheck::HP   => ["^[A-Z]{3}[0-9]{3}[0-9a-zA-Z]{4}$"], # 10
+
+    WarrantyCheck::HP   => ["^[a-zA-Z]{3}[0-9]{3}[0-9a-zA-Z]{4}$"], # 10
     WarrantyCheck::DELL => ["^[0-9a-zA-Z]{5,7}$"], # 5-7
     WarrantyCheck::IBM  => ["^[0-9a-zA-Z]{7}$", "^[0-9a-zA-Z]{10}$", "^[0-9a-zA-Z]{12}$"], # 7,10,12
   }
